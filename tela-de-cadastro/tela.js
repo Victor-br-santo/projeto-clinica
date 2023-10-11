08ef121389f9e630858a53f1d7e356ca62db863a
Victor-br-santo <victorbrsanto@Gmail.com>
2023-10-11 11:17:36 -0300

    segundo commit

--- tela-de-cadastro/tela.js
+++ tela-de-cadastro/tela.js
@@ -1,1 +1,10 @@
-//vazio
+function mostrarSegundaParte() {
+    const idadeSelecionada = document.getElementById("idade").value;
+    const segundaParte = document.querySelector(".segunda-parte");
+
+    if (idadeSelecionada === "menor") {
+        segundaParte.style.display = "block";
+    } else {
+        segundaParte.style.display = "none";
+    }
+}
